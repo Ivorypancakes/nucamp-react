dee2e8c171148e696a124d0ab3c2cafceb01c4a2
caitlin mcgraw <caitlinmcg10@gmail.com>
2021-11-17 16:08:00 -0700

    fourth commit

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -22,8 +22,10 @@ class CampsiteInfo extends Component {
         console.log(this.props);
         if(this.props.campsite){
             return (
-                <div className="row">
-                    {this.renderCampsite(this.props.campsite)} {this.renderComments(this.props.campsite.comments)}
+                <div className="container">
+                    <div className="row">
+                        {this.renderCampsite(this.props.campsite)} {this.renderComments(this.props.campsite.comments)}
+                    </div>
                 </div>
             );
         } else {
